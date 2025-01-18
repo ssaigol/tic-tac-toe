cd4cdd698200c120184d3e56e6bcb5a125e5a889
Saif Saigol <96741176+ssaigol@users.noreply.github.com>
2025-01-18 18:28:39 -0500

    Add logic for gameplay from console

--- script.js
+++ script.js
@@ -12,24 +12,61 @@ const Gameboard = (function () {
         return {addValue, getValue};
     }
     
-    for (let i = 0; i < rows; i++) {
-        boardArr[i] = [];
-        for (let j = 0; j < columns; j++) {
-            boardArr[i].push(Cell());
+    const newBoard = function () {
+        for (let i = 0; i < rows; i++) {
+            boardArr[i] = [];
+            for (let j = 0; j < columns; j++) {
+                boardArr[i].push(Cell());
+            }
+        };
+    };
+
+    newBoard();
+
+
+    const getBoard = () => {
+        const arr = [];
+        for (let i = 0; i < rows; i++) {
+            arr[i] = [];
+            for (let j = 0; j < columns; j++) {
+                arr[i][j] = boardArr[i][j].getValue();
+            }
         }
+        return arr;
     }
+    
 
-    const getBoard = () => boardArr; 
 
-    const playMove = (row, column, player) => {
-        boardArr[row][column].addValue(player);
-        return console.log(`Cell ${row}, ${column}: ${player}`);
+    const playMove = function (row, column) {
+        if (boardArr[row][column].getValue() === "X" || boardArr[row][column].getValue() === "O") {
+            return console.log("Cell Occupied");
+        } else boardArr[row][column].addValue(this.token);
+        Gameplay.gameWon();
+        Gameplay.gameTied();
+        return console.log(getBoard());
     }
 
     const getCellValue = (row, column) => boardArr[row][column].getValue();
 
-    return {getBoard, playMove, getCellValue};
+    return {getBoard, playMove, getCellValue, newBoard};
 
+})();
+
+
+const Players = (function () {
+    const players = [
+        {name: "Player One", token: "X"},
+        {name: "Player Two", token: "O"}
+    ];
+
+    players.forEach(player => {
+        Object.assign(player, {playMove: Gameboard.playMove.bind(player)});
+    });
+
+    const playerName = (index) => players[index].name;
+    const playerToken = (index) => players[index].token
+    
+    return {playerName, playerToken};
 })();
 
 
@@ -37,21 +74,66 @@ const Gameboard = (function () {
 
 
 
+const Gameplay = (function () {
+    const Player1 = Object.assign({}, {
+        name: Players.playerName(0),
+        token: Players.playerToken(0),
+    });
+    const Player2 = Object.assign({}, {
+        name: Players.playerName(1),
+        token: Players.playerToken(1),
+    });
+
+    Player1.playMove = Gameboard.playMove.bind(Player1); 
+    Player2.playMove = Gameboard.playMove.bind(Player2); 
+
+    const displayPlayers = () => console.log(Player1, Player2);
+
+    const player1Move = (x, y) => Player1.playMove(x, y);
+    const player2Move = (x, y) => Player2.playMove(x, y);
+
+    const gameWon = () => {
+        if (Gameboard.getCellValue(0, 0) === Gameboard.getCellValue(0, 1) && Gameboard.getCellValue(0, 0) === Gameboard.getCellValue(0, 2) && Gameboard.getCellValue(0, 0) != null) {
+            Gameboard.getBoard();
+            Gameboard.newBoard();
+            return console.log("Game won!");
+        }
+        if (Gameboard.getCellValue(1, 0) === Gameboard.getCellValue(1, 1) && Gameboard.getCellValue(1, 0) === Gameboard.getCellValue(1, 2) && Gameboard.getCellValue(1, 0) != null) {
+            Gameboard.getBoard();
+            Gameboard.newBoard();
+            return console.log("Game won!");
+        }
+        if (Gameboard.getCellValue(2, 0) === Gameboard.getCellValue(2, 1) && Gameboard.getCellValue(2, 0) === Gameboard.getCellValue(2, 2) && Gameboard.getCellValue(2, 0) != null) {
+            Gameboard.getBoard();
+            Gameboard.newBoard();
+            return console.log("Game won!");
+        }
+        if (Gameboard.getCellValue(0, 0) === Gameboard.getCellValue(1, 1) && Gameboard.getCellValue(0, 0) === Gameboard.getCellValue(2, 2) && Gameboard.getCellValue(0, 0) != null) {
+            Gameboard.getBoard();
+            Gameboard.newBoard();
+            return console.log("Game won!");
+        }
+        if (Gameboard.getCellValue(0, 2) === Gameboard.getCellValue(1, 1) && Gameboard.getCellValue(0, 2) === Gameboard.getCellValue(2, 0) && Gameboard.getCellValue(0, 2) != null) {
+            Gameboard.getBoard();
+            Gameboard.newBoard();
+            return console.log("Game won!");
+        }
+    };
+
+    const gameTied = () => {
+        const arr = Array.from(Gameboard.getBoard());
+
+        if (arr.every(row => row.every(cell => cell === "X" || cell === "O"))) {
+            Gameboard.getBoard();
+            Gameboard.newBoard();
+            console.log("Game tied!");
+        };
+
+        
+    }
 
 
 
+    return {displayPlayers, player1Move, player2Move, gameWon, gameTied};
+})();
 
-
-
-
-
-
-
-
-const Players = function () {
-    return {};
-}
-
-const Gameplay = function () {
-    return {};
-}
