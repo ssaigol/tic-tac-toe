079f99c451a7e150a19315294f7d9bc8a17e8ebf
Saif Saigol <96741176+ssaigol@users.noreply.github.com>
2025-01-20 18:42:04 -0500

    Clean up code & functionality. Add game status element

--- script.js
+++ script.js
@@ -21,9 +21,6 @@ const Gameboard = (function () {
         };
     };
 
-    newBoard();
-
-
     const getBoard = () => {
         const arr = [];
         for (let i = 0; i < rows; i++) {
@@ -37,14 +34,8 @@ const Gameboard = (function () {
     
 
 
-    const playMove = function (row, column) {
-        if (boardArr[row][column].getValue() === "X" || boardArr[row][column].getValue() === "O") {
-            return console.log("Cell Occupied");
-        } else boardArr[row][column].addValue(this.token);
-        Gameplay.gameWon();
-        Gameplay.gameTied();
-        displayCache();
-        return console.log(getBoard());
+    const playMove = function (row, column, token) {
+        boardArr[row][column].addValue(token);
     }
 
     const getCellValue = (row, column) => boardArr[row][column].getValue();
@@ -60,93 +51,196 @@ const Players = (function () {
         {name: "Player Two", token: "O"}
     ];
 
-    players.forEach(player => {
-        Object.assign(player, {playMove: Gameboard.playMove.bind(player)});
-    });
-
-    const playerName = (index) => players[index].name;
-    const playerToken = (index) => players[index].token
+    const getPlayerName = (index) => players[index].name;
+    const getPlayerToken = (index) => players[index].token;
+    const namePlayers = (name1, name2) => {
+        if (name1 != "" && name2 != "") {
+            players[0].name = name1;
+            players[1].name = name2;
+        };
+    }
     
-    return {playerName, playerToken};
+    return {getPlayerName, getPlayerToken, namePlayers};
 })();
 
 
 const Gameplay = (function () {
-    const Player1 = Object.assign({}, {
-        name: Players.playerName(0),
-        token: Players.playerToken(0),
-    });
-    const Player2 = Object.assign({}, {
-        name: Players.playerName(1),
-        token: Players.playerToken(1),
-    });
-
-    Player1.playMove = Gameboard.playMove.bind(Player1); 
-    Player2.playMove = Gameboard.playMove.bind(Player2); 
-
-    const displayPlayers = () => console.log(Player1, Player2);
+    const Player1 = {
+        name: Players.getPlayerName(0),
+        token: Players.getPlayerToken(0),
+        playMove(row, column) {
+            Gameboard.playMove(row, column, this.token);
+        }
+    };
+    const Player2 = {
+        name: Players.getPlayerName(1),
+        token: Players.getPlayerToken(1),
+        playMove(row, column) {
+            Gameboard.playMove(row, column, this.token);
+        }
+    };
 
     const player1Move = (x, y) => Player1.playMove(x, y);
     const player2Move = (x, y) => Player2.playMove(x, y);
 
-    const gameWon = () => {
+    const gameWon = () => {         //CAN BE OPTIMIZED!! REVIEW
+        //Check if 3 across top row
         if (Gameboard.getCellValue(0, 0) === Gameboard.getCellValue(0, 1) && Gameboard.getCellValue(0, 0) === Gameboard.getCellValue(0, 2) && Gameboard.getCellValue(0, 0) != null) {
-            Gameboard.getBoard();
-            Gameboard.newBoard();
-            return console.log("Game won!");
+            return true;
         }
+        //Check if 3 across middle row
         if (Gameboard.getCellValue(1, 0) === Gameboard.getCellValue(1, 1) && Gameboard.getCellValue(1, 0) === Gameboard.getCellValue(1, 2) && Gameboard.getCellValue(1, 0) != null) {
-            Gameboard.getBoard();
-            Gameboard.newBoard();
-            return console.log("Game won!");
+            return true;
         }
+        //Check if 3 across bottom row
         if (Gameboard.getCellValue(2, 0) === Gameboard.getCellValue(2, 1) && Gameboard.getCellValue(2, 0) === Gameboard.getCellValue(2, 2) && Gameboard.getCellValue(2, 0) != null) {
-            Gameboard.getBoard();
-            Gameboard.newBoard();
-            return console.log("Game won!");
+            return true;
         }
+        //Check if 3 across left column
+        if (Gameboard.getCellValue(0, 0) === Gameboard.getCellValue(1, 0) && Gameboard.getCellValue(0, 0) === Gameboard.getCellValue(2, 0) && Gameboard.getCellValue(0, 0) != null) {
+            return true;
+        }
+        //Check if 3 across middle column
+        if (Gameboard.getCellValue(0, 1) === Gameboard.getCellValue(1, 1) && Gameboard.getCellValue(0, 1) === Gameboard.getCellValue(2, 1) && Gameboard.getCellValue(0, 1) != null) {
+            return true;
+        }
+        //Check if 3 across right column
+        if (Gameboard.getCellValue(0, 2) === Gameboard.getCellValue(1, 2) && Gameboard.getCellValue(0, 2) === Gameboard.getCellValue(2, 2) && Gameboard.getCellValue(0, 2) != null) {
+            return true;
+        }
+        //Check if 3 across back diagonal
         if (Gameboard.getCellValue(0, 0) === Gameboard.getCellValue(1, 1) && Gameboard.getCellValue(0, 0) === Gameboard.getCellValue(2, 2) && Gameboard.getCellValue(0, 0) != null) {
-            Gameboard.getBoard();
-            Gameboard.newBoard();
-            return console.log("Game won!");
+            return true;
         }
+        //Check if 3 across forward diagonal
         if (Gameboard.getCellValue(0, 2) === Gameboard.getCellValue(1, 1) && Gameboard.getCellValue(0, 2) === Gameboard.getCellValue(2, 0) && Gameboard.getCellValue(0, 2) != null) {
-            Gameboard.getBoard();
-            Gameboard.newBoard();
-            return console.log("Game won!");
+            return true;
         }
+        return false;
     };
 
     const gameTied = () => {
-        const arr = Array.from(Gameboard.getBoard());
-
-        if (arr.every(row => row.every(cell => cell === "X" || cell === "O"))) {
-            Gameboard.getBoard();
-            Gameboard.newBoard();
-            console.log("Game tied!");
+        if (Gameboard.getBoard().every(row => row.every(cell => cell === "X" || cell === "O"))) {
+            if (!gameWon()) return true;
         };
 
         
     }
 
+    const checkGame = () => {
+        gameWon();
+        gameTied();
+        if (gameWon()) {
+            return "Game won"
+        } else if (gameTied()) return "Game tied";
+    }
 
 
-    return {displayPlayers, player1Move, player2Move, gameWon, gameTied};
+    return {checkGame, player1Move, player2Move};
 })();
 
-
-const displayCache = function () {
-    const gameboard = document.getElementById("gameboard");
+const Display = (function () {
+    //DOM queries
     const cells = document.getElementsByClassName("cells");
-    for (i = 0; i < cells.length; i++) {
-        const cellRow = cells[i].dataset.row;
-        const cellColumn = cells[i].dataset.column;
-        // const cellContent = document.createElement("div");
-        // cellContent.textContent = Gameboard.getCellValue(cellRow, cellColumn);
-        // cells[i].appendChild(cellContent);
-        cells[i].textContent = Gameboard.getCellValue(cellRow, cellColumn);
+    const cellsArr = Array.from(cells);
+    const newGame = document.getElementById("new-game");
+    const dialog = document.getElementById("new-game-dialog");
+    const newGameSubmit = document.getElementById("modal-submit");
+    const player1Input = document.getElementById("player1-input");
+    const player2Input = document.getElementById("player2-input");
+    const player1Display = document.getElementById("player1");
+    const player2Display = document.getElementById("player2");
+    const gameStatus = document.getElementById("game-status");
+
+    //Variable for keeping track of current player - used to alternate turns, and change cell hover color
+    let currentPlayer;
+
+    //newGameEvent function
+    const newGameEvent = () => {
+        //Name players with inputed names from dialog
+        const player1Name = player1Input.value;
+        const player2Name = player2Input.value;
+        Players.namePlayers(player1Name, player2Name);
+        //Attach inputed names to displays below Gameboard display
+        player1Display.value = `${Players.getPlayerName(0)}: X`;
+        player2Display.value = `${Players.getPlayerName(1)}: O`;
+        dialog.close();   //Close dialog
+        Gameboard.newBoard(); //Create new board with empty Cells
+        currentPlayer = "Player 1"; //Sets current player to Player 1 (or equivalent name) at start of new game
+        cellsArr.forEach(cell => {                    //Creates array of each Cell element from cells HTML collection
+            cell.addEventListener("click", clickMove);         //attaches click event listener to each Cell, triggers clickMove function
+        });
     }
-};
+
+    //clickMove function
+    const clickMove = (e) => {
+        const cell = e.target;
+        const cellRow = cell.dataset.row;
+        const cellColumn = cell.dataset.column;
+
+        if (Gameboard.getCellValue(cellRow, cellColumn) === null && currentPlayer === "Player 1") {
+            Gameplay.player1Move(cellRow, cellColumn);
+            currentPlayer = "Player 2";
+        } else if (Gameboard.getCellValue(cellRow, cellColumn) === null && currentPlayer === "Player 2") {
+            Gameplay.player2Move(cellRow, cellColumn);
+            currentPlayer = "Player 1";
+        } else if (Gameboard.getCellValue(cellRow, cellColumn) != null) {
+            alert("Cell occupied");
+        }
+
+        render(); //Update board display
+
+        Gameplay.checkGame(); //Check if game over (won or tied)
+    };
+
+    //render function
+    const render = () => {
+        cellsArr.forEach(cell => {
+            const cellRow = cell.dataset.row;
+            const cellColumn = cell.dataset.column;
+            const cellValue = Gameboard.getCellValue(cellRow, cellColumn);
+            cell.textContent = cellValue;
+
+            if (Gameboard.getBoard().every(row => row.every(cell => cell === null))) {
+                cell.classList.add("player1-hover");    
+            } else cell.classList.remove('player1-hover', 'player2-hover');
+
+            if (currentPlayer === "Player 1") {
+                cell.classList.add("player1-hover");
+            } else if (currentPlayer === "Player 2") {
+                cell.classList.add("player2-hover");
+            };
+        });
+        
+        let winningPlayer;
+        if (currentPlayer === "Player 1") {
+            winningPlayer = Players.getPlayerName(1);
+        } else winningPlayer = Players.getPlayerName(0);
+        if (Gameplay.checkGame() === "Game won") {
+            gameStatus.textContent = `${winningPlayer} won!`;
+            cellsArr.forEach(cell => {
+                cell.removeEventListener("click", clickMove);
+                cell.classList.remove("player1-hover", "player2-hover");
+            });
+        } else if (Gameplay.checkGame() === "Game tied") {
+            gameStatus.textContent = "Game tied! No winner";
+            cellsArr.forEach(cell => {
+                cell.removeEventListener("click", clickMove);
+                cell.classList.remove("player1-hover", "player2-hover");
+            });
+        } else if (currentPlayer === "Player 1") {
+            gameStatus.textContent = `${Players.getPlayerName(0)}'s turn`;
+        } else if (currentPlayer === "Player 2") {
+            gameStatus.textContent = `${Players.getPlayerName(1)}'s turn`;
+        }
 
 
+    };
 
+    //Event listeners
+    newGame.addEventListener("click", () => dialog.showModal()); //opens New Game dialog when button clicked
+    window.addEventListener("click", dialog.showModal()); //opens New Game dialog when page loads
+    newGameSubmit.addEventListener("click", newGameEvent); //triggers newGameEvent when New Game dialog submit button clicked
+    newGameSubmit.addEventListener("click", render);
+   
+})();
